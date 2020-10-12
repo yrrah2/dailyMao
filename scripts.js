d3119771d7f5e9b2535e063dbd83dad5c45236b0
Harry Hackett <yrrah2@users.noreply.github.com>
2020-10-13 08:09:57 +1300

    More accurate formula to choose number in a range

--- scripts.js
+++ scripts.js
@@ -43,9 +43,9 @@ const generateQuote = (minQuoteVal, maxQuoteVal) => {
                 success: (data) => {
                         var quoteArray = CSVToArray(data, ',');                         // Convert quote CSV to Array
                         
-                        var range = maxQuoteVal - minQuoteVal                           // Find number of values between selected range
-                        var rnd = minQuoteVal + Math.floor(Math.random() * range);    // Choose random quote in range
-                        var chosenQuote = quoteArray[rnd]                               // Save randomly chosen quote
+                        var range = maxQuoteVal - minQuoteVal + 1;                      // Find number of values between selected range
+                        var rnd =  Math.floor(Math.random() * range) + minQuoteVal;     // Choose random quote in range
+                        var chosenQuote = quoteArray[rnd];                              // Save randomly chosen quote
 
                         $("#quote_chapter").text(chosenQuote[2]);                       // Display the chapter the quote appears in
                         $("#quote_reference").text(chosenQuote[3]+':'+chosenQuote[4]);  // Display the quote reference
