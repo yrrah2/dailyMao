66f3122690c23291077f60b1f800515576585190
Harry Hackett <yrrah2@users.noreply.github.com>
2020-10-13 10:03:10 +1300

    Better random from range script

--- scripts.js
+++ scripts.js
@@ -1,12 +1,12 @@
 const generateQuote = (minQuoteVal, maxQuoteVal) => {
-                        var range = maxQuoteVal - minQuoteVal + 1;                      // Find number of values between selected range
-                        var rnd =  Math.floor(Math.random() * range) + minQuoteVal;     // Choose random quote in range
-                        var chosenQuote = quoteArray[rnd];                              // Save randomly chosen quote
+        var quoteRange = quoteArray.slice(minQuoteVal, maxQuoteVal);    // Select range
+        var rnd = Math.floor(Math.random() * quoteRange.length);        // Choose random quote from range
+        const chosenQuote = quoteRange[rnd];                            // Declare chosen quote
 
-                        $("#quote_chapter").text(chosenQuote[2]);                       // Display the chapter the quote appears in
-                        $("#quote_reference").text(chosenQuote[3]+':'+chosenQuote[4]);  // Display the quote reference
-                        $("#quote_proper").text(chosenQuote[0]);                        // Display the quote itself
-                        $("#quote_source").text(chosenQuote[1]);                        // Display the source of the quote as listed in QfMZ
+        $("#quote_chapter").text(chosenQuote[2]);                       // Display the chapter the quote appears in
+        $("#quote_reference").text(chosenQuote[3]+':'+chosenQuote[4]);  // Display the quote reference
+        $("#quote_proper").text(chosenQuote[0]);                        // Display the quote itself
+        $("#quote_source").text(chosenQuote[1]);                        // Display the source of the quote as listed in QfMZ
 };
 
 const pageload = () => { 
